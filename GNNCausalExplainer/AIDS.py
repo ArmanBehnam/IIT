9bd4794928d3d4f1db4c785858aa8af39cd0e37e
Arman Behnam <45828755+ArmanBehnam@users.noreply.github.com>
2023-06-13 18:25:16 -0500

    Update AIDS.py

--- GNNCausalExplainer/AIDS.py
+++ GNNCausalExplainer/AIDS.py
@@ -1,13 +1,9 @@
 import math
-import alg1
-from alg3 import *
-import causal
 import pandas as pd
-from causal import *
 import torch
 from itertools import product
 
-AIDS = "D:/University/Spring2023/Research/Session23-230608/Codes/My/data/AIDS/"
+AIDS = "/content/drive/MyDrive/data/AIDS/"
 AIDS_df = pd.read_csv(AIDS + 'AIDS_A.txt', sep=',', header=None, names=['from', 'to'])
 AIDS_graph_indicator = pd.read_csv(AIDS + 'AIDS_graph_indicator.txt', header=None, names=['graph_id'])
 AIDS_node_labels = pd.read_csv(AIDS + 'AIDS_node_labels.txt', header=None, names=['node_label'])
@@ -27,7 +23,7 @@ print(f"Target node: {v_star}")
 print(f"1-hop neighbors of A: {one_hop_neighbors}")
 print(f"2-hop neighbors of A: {two_hop_neighbors}")
 print(f"Out of neighborhood of A: {out_of_neighborhood}")
-cg.plot()
+# cg.plot()
 
 # hyperparameters
 num_epochs = 2
@@ -37,13 +33,26 @@ num_layers = [1, 2, 3, 4]
 lambdas = [0.01, 0.05, .1,.2,.3]
 hyperparameters = product(learning_rates, hidden_sizes, num_layers, lambdas)
 total_loss = []
+min_loss = float('inf')
+best_hyperparams = None
 for i, hyperparams in enumerate(hyperparameters):
     learning_rate, h_size, h_layers, lambdas = hyperparams
     print(f'Training with learning rate: {learning_rate}, h_size: {h_size}, h_layers: {h_layers}, lambdas: {lambdas}','\n')
-    causal_loss = alg1.train(cg, lambdas, learning_rate, h_size, h_layers, num_epochs)
+    causal_loss,best_ncm_model,p_do = alg1.train(cg, lambdas, learning_rate, h_size, h_layers, num_epochs)
     total_loss.append(causal_loss)
-total_loss = [x for x in total_loss if not math.isnan(x[0])]
-print(total_loss)
+    if causal_loss < min_loss:
+        best_model = best_ncm_model
+        best_intervention = p_do
+        min_loss = causal_loss
+        best_hyperparams = hyperparams
+
+print('The minimum loss obtained is:', min_loss)
+print('The best hyperparameters are:',f'Training with learning rate: {best_hyperparams[0]}, h_size: {best_hyperparams[1]}, h_layers: {best_hyperparams[2]}, lambdas: {best_hyperparams[3]}','\n')
+print('The best model is: ', best_model,'and its identified query value is: ',best_intervention.data)
+print(f"Target node: {best_model.graph.target_node}")
+print(f"1-hop neighbors of A: {best_model.graph.one_hop_neighbors}")
+print(f"2-hop neighbors of A: {best_model.graph.two_hop_neighbors}")
+print(f"Out of neighborhood of A: {best_model.graph.out_of_neighborhood}")
 
 plt.figure()
 plt.plot(total_loss)
@@ -51,4 +60,4 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.title('Loss over epochs')
 plt.savefig('AIDS NCM Loss over epochs.png')
-plt.show()
+# plt.show()
